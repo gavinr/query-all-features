de4e7150acea235adb3d5c6bb867b975c967d9a1
Gavin Rehkemper <gavin@gavinr.com>
2022-04-29 20:21:10 -0500

    fix naming

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
 import { queryFeatures } from "@esri/arcgis-rest-feature-service";
 
-export const getAllFeatures = (url) => {
+export const queryAllFeatures = (url) => {
   // first query the capabilities to get the maxRecordCount
-  return _getAllFeaturesRecursive(url, []);
+  return _recurse(url, []);
 };
 
 // Recursive function - Handles calling the service multiple times if necessary.
-const _getAllFeaturesRecursive = (url, featuresSoFar) => {
+const _recurse = (url, featuresSoFar) => {
   return queryFeatures({
     url: url,
     resultOffset: featuresSoFar.length,
     resultRecordCount: 1000, /// layer.capabilities.query.maxRecordCount,
     // where: "st = 'CA'",
-    outFields: ['*']
-  })
-    .then((results) => {
-    // If "exceededTransferLimit" is true, then make another request (call 
-    //  this same function) with a new "start" position. If not, we're at the end
-    // and we should just concatenate the results and return what we have.
-    if (
-      "exceededTransferLimit" in results &&
-      results.exceededTransferLimit === true
-    ) {
-      return _getAllFeaturesRecursive(url, [
-        ...featuresSoFar,
-        ...results.features
-      ]);
-    } else if("features" in results) {
-      const featureSetClone = JSON.parse(JSON.stringify(results));
-      featureSetClone.features = [...featuresSoFar, ...results.features]
-      return Promise.resolve(featureSetClone);
-    } else {
+    outFields: ["*"],
+  }).then(
+    (results) => {
+      // If "exceededTransferLimit" is true, then make another request (call
+      //  this same function) with a new "start" position. If not, we're at the end
+      // and we should just concatenate the results and return what we have.
+      if (
+        "exceededTransferLimit" in results &&
+        results.exceededTransferLimit === true
+      ) {
+        return _recurse(url, [...featuresSoFar, ...results.features]);
+      } else if ("features" in results) {
+        const featureSetClone = JSON.parse(JSON.stringify(results));
+        featureSetClone.features = [...featuresSoFar, ...results.features];
+        return Promise.resolve(featureSetClone);
+      } else {
+        return Promise.reject(err);
+      }
+    },
+    (err) => {
       return Promise.reject(err);
     }
-  }, (err) => {
-    return Promise.reject(err);
-  });
+  );
 };
